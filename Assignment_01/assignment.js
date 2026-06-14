0d410a05f0a6c7fd01202d853ac24b1c50589944
m-mubashir987 <satorumubashir987@gmail.com>
2026-06-14 12:21:05 +0500

    Add files via upload

---
+++ Assignment_01/assignment.js
@@ -0,0 +1,18 @@
+var studentName = 'mubashir' 
+undefined
+scheduler
+Scheduler {}
+studentName
+'mubashir'
+var studentAge = 16
+undefined
+studentAge
+16
+var studentEmail = 'mub@gmail.com'
+undefined
+studentEmail
+'mub@gmail.com'
+var studentPassword = 'sir  arham'
+undefined
+studentPassword
+'sir  arham'
---
+++ Assignment_01/assingment.py
@@ -0,0 +1,9 @@
+studentName = 'mubashir';
+studentAge = 16;
+studentEmail = 'mub@gmail.com';
+studentPassword = 'sir arham';
+
+print(studentName)
+print(studentAge)
+print(studentEmail)
+print(studentPassword)
